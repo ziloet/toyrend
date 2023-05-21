3ca3d302e07cb85f4af6dd8a8d9ab3db68e9279a
Lazuli Ziloet <rneutron@protonmail.com>
2023-05-21 16:39:32 +0300

    link gdi32.lib

--- build.bat
+++ build.bat
@@ -7,6 +7,6 @@ if not exist "build" (
 pushd "build"
 set compiler_flags=/nologo /W3 /Zi /Fe:main.exe
 set linker_flags=/incremental:no /subsystem:windows /WX /opt:icf /opt:ref
-set libs_to_link=kernel32.lib user32.lib
+set libs_to_link=kernel32.lib user32.lib gdi32.lib
 call cl %compiler_flags% ../main.c /link %linker_flags% %libs_to_link%
 popd
