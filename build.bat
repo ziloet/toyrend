be772c2c9febcc75b2ce5e9e66277b540d3865fd
Lazuli Ziloet <rneutron@protonmail.com>
2023-05-29 23:29:20 +0300

    rename: main.c -> toyrend_main_win32.c

--- build.bat
+++ build.bat
@@ -5,8 +5,8 @@ if not exist "build" (
 )
 
 pushd "build"
-set compiler_flags=/nologo /W3 /Zi /Fe:main.exe
+set compiler_flags=/nologo /W3 /Zi /Fe:toyrend_win32.exe
 set linker_flags=/incremental:no /subsystem:windows /WX /opt:icf /opt:ref
 set libs_to_link=kernel32.lib user32.lib gdi32.lib
-call cl %compiler_flags% ../main.c /link %linker_flags% %libs_to_link%
+call cl %compiler_flags% ../toyrend_main_win32.c /link %linker_flags% %libs_to_link%
 popd
